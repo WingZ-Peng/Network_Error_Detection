9f6f7bc0ac6c3a91089b278d0917ac871d00e036
Zhipeng Peng <74479094+WingZ-Peng@users.noreply.github.com>
2024-09-26 16:10:47 -0700

    Add files via upload

--- error_detection.cpp
+++ error_detection.cpp
@@ -23,7 +23,7 @@ string XOR(string a, string b){
     return ret;
 }
 
-string calculateCRC(string& dividend, string& divisor){
+string calculateCRC(string dividend, string divisor){
     int idx = divisor.size();
     string tmp_dividend = dividend.substr(0, idx);
 
@@ -47,7 +47,7 @@ string calculateCRC(string& dividend, string& divisor){
     return tmp_dividend;
 }
 
-string CRC(string& data){
+string CRC(string data){
     string CRC16 = "10000010000010001"; 
     int length_of_zero = CRC16.size() - 1;
     // append correct numbers of zero to data
@@ -58,7 +58,7 @@ string CRC(string& data){
 }
 
 // CheckSum
-vector<string> separateToParts(const string& data, int partSize = 16) {
+vector<string> separateToParts(string data, int partSize = 16) {
     vector<string> parts;
     for (int i = 0; i < 4; ++i) {
         parts.push_back(data.substr(i * partSize, partSize));
@@ -66,7 +66,7 @@ vector<string> separateToParts(const string& data, int partSize = 16) {
     return parts;
 }
 
-vector<int> convertBinaryToDecimal(const vector<string>& binaryParts) {
+vector<int> convertBinaryToDecimal(vector<string> binaryParts) {
     vector<int> decimalParts;
     for (const auto& part : binaryParts) {
         decimalParts.push_back(bitset<16>(part).to_ulong());
@@ -74,7 +74,7 @@ vector<int> convertBinaryToDecimal(const vector<string>& binaryParts) {
     return decimalParts;
 }
 
-int calculateSum(const vector<int>& numbers) {
+int calculateSum(vector<int> numbers) {
     int sum = 0;
     for (const auto& n : numbers) {
         sum += n;
@@ -94,13 +94,51 @@ string calculateChecksum(int sum, int divisor = 65536) {
     return checksum;
 }
 
-string CheckSum(string& data) {
+string CheckSum(string data) {
     vector<string> binary_parts = separateToParts(data);
     vector<int> decimal_parts = convertBinaryToDecimal(binary_parts);
     int bit16_sum = calculateSum(decimal_parts);
     string check_code = calculateChecksum(bit16_sum);
     
     return check_code;
+}
+
+string introduceErrorBits(string data, string errorBits){
+    string polluted_data = data;
+    for (int i = 0; i < data.size(); ++i){
+        if (errorBits[i] == '1') {
+            polluted_data[i] = (polluted_data[i] == '0') ? '1' : '0';
+        }
+    }
+    return polluted_data;
+}
+
+void printResult(string data, string errorBits){
+    // Append the check codes to the data following the corresponding rules
+    string crc_code = CRC(data);
+    string checksum_code = CheckSum(data);
+    string crc_data = data + crc_code;
+    string checksum_data = data + checksum_code;
+
+    // Introduce error, length is 80 bits
+    string polluted_crc = introduceErrorBits(crc_data, errorBits);
+    string polluted_checksum = introduceErrorBits(checksum_data, errorBits);
+
+    // recalculate the remainder of CRC & CheckSum
+    string polluted_crc_code = CRC(polluted_crc.substr(0, data.size()));
+    string polluted_checksum_code = CheckSum(polluted_checksum.substr(0, data.size()));
+
+    // Print result for CRC
+    cout << "CRC-16" << endl;
+    cout << "CRC: " << crc_code << "; \t\tResult: " 
+         << ((crc_code == polluted_crc_code) ? "Pass" : "Not Pass") 
+         << endl;
+
+    // Print results for checksum
+    cout << "Checksum" << endl;
+    cout << "Checksum: " << checksum_code << "; \tResult: " 
+         << ((checksum_code == polluted_checksum_code) ? "Pass" : "Not Pass") 
+         << endl;
 }
 
 int main(){
@@ -111,19 +149,17 @@ int main(){
         cout<<"File does not exist.";
     }
     else{
-        string input, data, error;
+        string input, data, errorBits;
         
         while (getline(myfile, input)){
             for (int i = 0; i < input.length(); ++i){
                 if (input[i] == ' '){
                     data = input.substr(0, i);
-                    error = input.substr(i+1);
-                    // cout << data << "---" << error << endl;
-                    
-                    string a = CRC(data);
-                    string b = CheckSum(data);
-                    cout << "CRC remainder is: " << a << endl;
-                    cout << "Checksum remainder is: " << b << endl;
+                    errorBits = input.substr(i+1);
+                    cout << "=================================" << endl;
+                    cout << "Data: " << data << endl;
+                    cout << "Error: " << errorBits << endl;
+                    printResult(data, errorBits);
                     break;
                 }
             }
